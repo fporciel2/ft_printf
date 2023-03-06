36d3f8aec216c80b13d72bd04efd6d3e05985325
Federico Porciello <fporciel@e4r7p4.42roma.it>
2023-03-06 10:26:19 +0100

    Mini parsing.

--- ft_is_format_spec.c
+++ ft_is_format_spec.c
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_is_format_spec.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/06 09:48:08 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/06 10:19:05 by fporciel         ###   ########.fr       */
+/*   Created: 2023/03/06 10:19:23 by fporciel          #+#    #+#             */
+/*   Updated: 2023/03/06 10:25:29 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
-# include <stdarg.h>
-# include "libft.h"
+#include "libftprintf.h"
 
-int	ft_printf(const char *format, ...);
-int	ft_is_format_spec(const char *format, int i);
-
-#endif
+int	ft_is_format_spec(const char *format, int i)
+{
+	i++;
+	if (format[i] == 37)
+		return (1);
+	else if ((format[i] == 'c') || (format[i] == 's') || (format[i] == 'p')
+			|| (format[i] == 'd') || (format[i] == 'i') || (format[i] == 'u')
+			|| (format[i] == 'x') || (format[i] == 'X'))
+		return (2);
+	else
+		return (0);
+}
