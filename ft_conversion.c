31e751c461f474af81ecabf3eb6c296279000cb0
Federico Porciello <fporciel@e4r7p4.42roma.it>
2023-03-06 11:39:54 +0100

    LOL

--- ft_conversion.c
+++ ft_conversion.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 11:08:13 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/06 11:09:45 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/06 11:37:43 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,21 @@
 
 int	ft_conversion(const char *format, int *i, va_list ap)
 {
+	int	result;
+
+	result = 0;
+	(*i)++;
+	if (format[(*i)] == 'c')
+		result = result + ft_putchar(va_arg(ap, int));
+	else if (format[(*i)] == 's')
+		result = result + ft_putstr(va_arg(ap, char *));
+	else if (format[(*i)] == 'p')
+		result = result + ft_putvoid(va_arg(ap, uintptr_t));
+	else if ((format[(*i)] == 'd') || (format[(*i)] == 'i'))
+		result = result + ft_putnbr(va_arg(ap, int));
+	else if (format[(*i)] == 'u')
+		result = result + ft_putunbr(va_arg(ap, unsigned int));
+	else if ((format[(*i)] == 'x') || (format[(*i)] == 'X'))
+		result = result + ft_puthexnbr(va_arg(ap, unsigned int), format[(*i)]);
+	return (result);
 }
